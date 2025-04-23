a274e1d0c8e8bbccd692a7ebee557ecaa362fcd5
mayankg6453 <mayankg6453@gmail.com>
2025-04-23 11:06:35 +0530

    implement authentication using firebase

--- firebase/client.ts
+++ firebase/client.ts
@@ -10,18 +10,17 @@ import { getApps, getApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyDI61NqVx3J5DQzJhgw2bUoOFt0y5JvqFE",
-	authDomain: "prepinter-8a011.firebaseapp.com",
-	projectId: "prepinter-8a011",
-	storageBucket: "prepinter-8a011.firebasestorage.app",
-	messagingSenderId: "123655649810",
-	appId: "1:123655649810:web:262b87494516c9cfeaf6cb",
-	measurementId: "G-C0Y875TXL4",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig): getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-
-export const  auth = getAuth(app);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
